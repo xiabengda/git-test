75b880e49e4e95c5f6ed70d216e1b2b9dc05f98c
jim <jim@qq.com>
2021-11-05 16:07:53 +0800

    first commit

---
+++ src/com/atguigu/java/GitTest.java
@@ -0,0 +1,17 @@
+package com.atguigu.java;
+
+/**
+ * @ProjectName git-test
+ * @Description TODO
+ * @Author jim
+ * @Time 2021/11/5 16:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
